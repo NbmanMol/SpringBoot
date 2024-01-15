c942066c292d4fd93910e64b506a39edf7f6af61
NbmanMol <125892325+NbmanMol@users.noreply.github.com>
2024-01-15 17:51:51 +0500

    Delete Bootstrap/PP_3_1_2_Boot_Security directory

--- Bootstrap/PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/utill/UserNotCreatedExeption.java
+++
@@ -1,7 +0,0 @@
-package ru.kata.spring.boot_security.demo.utill;
-
-public class UserNotCreatedExeption extends RuntimeException {
-    public UserNotCreatedExeption(String msg) {
-        super(msg);
-    }
-}
